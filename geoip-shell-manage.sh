ac39649501bd6b1c23c0a2809eaef195744a1977
amos <anton.khazan@gmail.com>
2024-02-26 03:47:13 +0200

    manage: - source owrt-common on OpenWrt - improved usage() - compacted some code - report_status(): support OpenWrt persistence checks and reporting

--- geoip-shell-manage.sh
+++ geoip-shell-manage.sh
@@ -10,6 +10,7 @@ script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 
 . "$script_dir/${p_name}-common.sh" || exit 1
 . "$script_dir/${p_name}-nft.sh" || exit 1
+[ "$_OWRT_install" ] && { . "$script_dir/${p_name}-owrt-common.sh" || exit 1; }
 
 export list_type nolog=1 manmode=1
 
@@ -29,32 +30,30 @@ Usage: $me <action> [-c <"country_codes">] [-s <"expression"|disable>]  [-p <por
 Provides interface to configure geoip blocking.
 
 Actions:
-
-on|off      : enable or disable the geoip blocking chain  (via a rule in the base geoip chain)
-add|remove  : add or remove country codes (ISO 3166-1 alpha-2) to/from geoip blocking rules
-apply       : apply current config settings. If used with option '-p', allows to change ports geoblocking applies to.
-schedule    : change the cron schedule
-status      : check on the current status of geoip blocking
-reset       : reset geoip config and firewall geoip rules
-restore     : re-apply geoip blocking rules from the config
-showconfig  : print the contents of the config file
+  on|off      : enable or disable the geoip blocking chain  (via a rule in the base geoip chain)
+  add|remove  : add or remove country codes (ISO 3166-1 alpha-2) to/from geoip blocking rules
+  apply       : apply current config settings. If used with option '-p', allows to change ports geoblocking applies to.
+  schedule    : change the cron schedule
+  status      : check on the current status of geoip blocking
+  reset       : reset geoip config and firewall geoip rules
+  restore     : re-apply geoip blocking rules from the config
+  showconfig  : print the contents of the config file
 
 Options:
-
--c <"country_codes">               : country codes (ISO 3166-1 alpha-2). if passing multiple country codes, use double quotes.
--s <"expression"|disable>          : schedule expression for the periodic cron job implementing auto-updates of the ip lists,
-                                         must be inside double quotes.
-                                         default schedule is "15 4 * * *" (at 4:15 [am] every day)
-                                     disable: skip creating the autoupdate cron job
--p <[tcp:udp]:[allow|block]:ports> : Only geoblock incoming traffic on specific ports,
-                                        or geoblock all traffic except traffic arriving on specific ports.
-                                        Multiple '-p' options are allowed.
-                                        For examples, refer to NOTES.md.
-                                        Only works with the 'apply' action.
--v                                 : Verbose status output
--f                                 : Force the action
--d                                 : Debug
--h                                 : This help
+  -c <"country_codes">               : country codes (ISO 3166-1 alpha-2). if passing multiple country codes, use double quotes.
+  -s <"expression"|disable>          : schedule expression for the periodic cron job implementing auto-updates of the ip lists,
+                                           must be inside double quotes.
+                                           default schedule is "15 4 * * *" (at 4:15 [am] every day)
+                                       disable: skip creating the autoupdate cron job
+  -p <[tcp:udp]:[allow|block]:ports> : For given protocol (tcp/udp), use "block" to only geoblock incoming traffic on specific ports,
+                                          or use "allow" to geoblock all incoming traffic except on specific ports.
+                                          Multiple '-p' options are allowed to specify both tcp and udp in one command.
+                                          Only works with the 'apply' action.
+                                          For examples, refer to NOTES.md.
+  -v                                 : Verbose status output
+  -f                                 : Force the action
+  -d                                 : Debug
+  -h                                 : This help
 
 EOF
 }
@@ -112,11 +111,15 @@ report_status() {
 		esac
 	}
 
+	warn_persist() {
+		printf '\n%s\n' "$WARN $1 Geoip${cr_p# and} $wont_work." >&2
+	}
+
 	incr_issues() { issues=$((issues+1)); }
 
-	V_sym="${green}✔${n_c}"
-	X_sym="${red}✘${n_c}"
-	Q_sym="${red}?${n_c}"
+	_V="${green}✔${n_c}"
+	_X="${red}✘${n_c}"
+	_Q="${red}?${n_c}"
 	issues=0
 
 	for entry in "Source ipsource" "WAN_ifaces wan_ifaces" "tcp tcp_ports" "udp udp_ports" \
@@ -128,7 +131,7 @@ report_status() {
 
 	printf '\n%s\n%s\n' "Geoip blocking mode: ${blue}${list_type}${n_c}" "Ip lists source: ${blue}${ipsource}${n_c}"
 
-	check_lists_coherence && lists_coherent=" $V_sym" || { report_incoherence; incr_issues; lists_coherent=" $Q_sym"; }
+	check_lists_coherence && lists_coherent=" $_V" || { report_incoherence; incr_issues; lists_coherent=" $_Q"; }
 
 	# check ipsets and firewall rules for active ccodes
 	for list_id in $active_lists; do
@@ -139,12 +142,12 @@ report_status() {
 	san_str active_families
 	printf %s "Country codes in the $list_type: "
 	case "$active_ccodes" in
-		'') printf '%s\n' "${red}None $X_sym"; incr_issues ;;
+		'') printf '%s\n' "${red}None $_X"; incr_issues ;;
 		*) printf '%s\n' "${blue}${active_ccodes}${n_c}${lists_coherent}"
 	esac
 	printf %s "IP families in firewall rules: "
 	case "$active_families" in
-		'') printf '%s\n' "${red}None${n_c} $X_sym" ;;
+		'') printf '%s\n' "${red}None${n_c} $_X" ;;
 		*) printf '%s\n' "${blue}${active_families}${n_c}${lists_coherent}"
 	esac
 
@@ -181,19 +184,19 @@ report_status() {
 	printf '\n'
 
 	curr_geotable="$(nft_get_geotable)" ||
-		{ echo "Error: failed to read the firewall state or firewall table $geotable does not exist." >&2; incr_issues; }
+		{ printf '%s\n' "$ERR failed to read the firewall state or firewall table $geotable does not exist." >&2; incr_issues; }
 
 	wl_rule="$(printf %s "$curr_geotable" | grep "drop comment \"${geotag}_whitelist_block\"")"
 
 	case "$(printf %s "$curr_geotable" | grep "jump $geochain comment \"${geotag}_enable\"")" in
-		'') chain_status="$X_sym"; incr_issues ;;
-		*) chain_status="$V_sym"
+		'') chain_status="$_X"; incr_issues ;;
+		*) chain_status="$_V"
 	esac
 	printf '%s\n' "Geoip firewall chain enabled: $chain_status"
 	[ "$list_type" = whitelist ] && {
 		case "$wl_rule" in
-			'') wl_rule=''; wl_rule_status="$X_sym"; incr_issues ;;
-			*) wl_rule="$_nl$wl_rule"; wl_rule_status="$V_sym"
+			'') wl_rule=''; wl_rule_status="$_X"; incr_issues ;;
+			*) wl_rule="$_nl$wl_rule"; wl_rule_status="$_V"
 		esac
 		printf '%s\n' "Whitelist blocking rule: $wl_rule_status"
 	}
@@ -205,7 +208,7 @@ report_status() {
 		printf "\n%s\n%s\n${fmt_str}%s\n" "${purple}Firewall rules in the $geochain chain${n_c}:" \
 			$dashes${blue} packets bytes ipv verdict prot dports interfaces extra $n_c$dashes
 		rules="$(nft_get_chain "$geochain" | sed 's/^[[:space:]]*//;s/ # handle.*//' | grep .)" ||
-			printf '%s\n' "${red}None $X_sym"
+			printf '%s\n' "${red}None $_X"
 		newifs "$_nl" rules
 		for rule in $rules; do
 			newifs ' "' wrds
@@ -234,7 +237,7 @@ report_status() {
 
 		printf '\n%s' "Ip ranges count in active geoip sets: "
 		case "$active_ccodes" in
-			'') printf '%s\n' "${red}None $X_sym" ;;
+			'') printf '%s\n' "${red}None $_X" ;;
 			*) printf '\n'
 				ipsets="$(nft -t list sets inet | grep -o ".._ipv._.*_$geotag")"
 				for ccode in $active_ccodes; do
@@ -244,7 +247,7 @@ report_status() {
 						get_matching_line "$ipsets" "" "${ccode}_${family}" "*" ipset
 						el_cnt=0
 						[ -n "$ipset" ] && el_cnt="$(nft_cnt_elements "$ipset")"
-						[ "$el_cnt" != 0 ] && list_empty='' || { list_empty=" $X_sym"; incr_issues; }
+						[ "$el_cnt" != 0 ] && list_empty='' || { list_empty=" $_X"; incr_issues; }
 						el_summary="$el_summary$family - $el_cnt$list_empty, "
 						total_el_cnt=$((total_el_cnt+el_cnt))
 					done
@@ -255,46 +258,65 @@ report_status() {
 
 	fi
 
+	unset cr_p
+	[ ! "$_OWRTFW" ] && cr_p=" and persistence across reboots"
+	wont_work="will likely not work" a_disabled="appears to be disabled"
+
 	# check if cron service is enabled
 	if check_cron; then
-		printf '\n%s\n' "Cron system service: $V_sym"
+		printf '\n%s\n' "Cron system service: $_V"
 
 		# check cron jobs
 
 		cron_jobs="$(crontab -u root -l 2>/dev/null)"
 
-		# check for persistence cron job
-		get_matching_line "$cron_jobs" "*" "${p_name}-persistence" "" persist_job
-		case "$persist_job" in
-			'') persist_job_status="$X_sym"; incr_issues ;;
-			*) persist_job_status="$V_sym"
-		esac
-		printf '%s\n' "Persistence cron job: $persist_job_status"
-
 		# check for autoupdate cron job
 		get_matching_line "$cron_jobs" "*" "${p_name}-autoupdate" "" update_job
 		case "$update_job" in
-			'') update_job_status="$X_sym"; upd_schedule=''; incr_issues ;;
-			*) update_job_status="$V_sym"; upd_schedule="${update_job%%\"*}"
+			'') upd_job_status="$_X"; upd_schedule=''; incr_issues ;;
+			*) upd_job_status="$_V"; upd_schedule="${update_job%%\"*}"
 		esac
-		printf '%s\n' "Autoupdate cron job: $update_job_status"
-		[ "$upd_schedule" ] && printf '%s\n\n' "Autoupdate schedule: '${blue}${upd_schedule% }${n_c}'"
+		printf '%s\n' "Autoupdate cron job: $upd_job_status"
+		[ "$upd_schedule" ] && printf '%s\n' "Autoupdate schedule: '${blue}${upd_schedule% }${n_c}'"
+
+		[ ! "$_OWRTFW" ] && {
+			# check for persistence cron job
+			get_matching_line "$cron_jobs" "*" "${p_name}-persistence" "" persist_job
+			case "$persist_job" in
+				'') persist_status="$_X"; incr_issues ;;
+				*) persist_status="$_V"
+			esac
+			printf '%s\n' "Persistence cron job: $persist_status"
+		}
 	else
-		printf '\n%s\n' "${yellow}WARNING${n_c}: cron service appears to be disabled. Persistence across reboots and autoupdates will likely not work." >&2
+		printf '\n%s\n' "$WARN cron service $a_disabled. Autoupdates$cr_p $wont_work." >&2
 		incr_issues
 	fi
+
+	[ "$_OWRTFW" ] && {
+		rv=0
+		printf %s "Persistence: "
+		check_owrt_init ||
+			{ rv=1; printf '%s\n' "$_X"; warn_persist "procd init script for $p_name $a_disabled."; incr_issues; }
+
+		check_owrt_include ||
+			{ [ $rv = 0 ] && printf '%s\n' "$_X"; rv=1; warn_persist "Firewall include is not found."; incr_issues; }
+		[ $rv = 0 ] && printf '%s\n' "$_V"
+	}
+
 	case $issues in
-		0) printf '%s\n\n' "${green}No problems detected.${n_c}" ;;
+		0) printf '\n%s\n\n' "${green}No problems detected.${n_c}" ;;
 		*) printf '\n%s\n\n' "${red}Problems detected: $issues.${n_c}"
 	esac
 }
 
 report_incoherence() {
-	printf '\n%s\n' "${red}Warning${n_c}: Discrepancy detected between the firewall state and the config file." >&2
+	discr="Discrepancy detected between"
+	printf '\n%s\n' "$WARN $discr the firewall state and the config file." >&2
 	for opt in unexpected missing; do
-		eval "[ \"\$${opt}_lists\" ] && printf '%s\n' \"$opt $list_type ip lists in the firewall: '\$${opt}_lists'\"" >&2
+		eval "[ \"\$${opt}_lists\" ] && printf '%s\n' \"$opt ip lists in the firewall: '\$${opt}_lists'\"" >&2
 	done
-	[ "$iplists_incoherent" ] && printf '%s\n' "${red}Warning${n_c}: Discrepancy detected between geoip ipsets and geoip firewall rules!" >&2
+	[ "$iplists_incoherent" ] && printf '%s\n' "$WARN $discr geoip ipsets and geoip firewall rules!" >&2
 }
 
 incoherence_detected() {
@@ -321,14 +343,14 @@ restore_from_config() {
 	check_reapply() {
 		check_lists_coherence && { echolog "Successfully re-applied previous $list_type ip lists."; return 0; }
 
-		echolog -err "Failed to re-apply previous $list_type lists." >&2
+		echolog -err "$FAIL re-apply previous $list_type lists." >&2
 		report_incoherence
 		return 1
 	}
 
 	echolog "Restoring lists '$config_lists_str' from the config file... "
 	case "$config_lists_str" in
-		'') echolog -err "Error: no ip lists registered in the config file." ;;
+		'') echolog -err "$ERR no ip lists registered in the config file." ;;
 		*) call_script "$script_dir/${p_name}-uninstall.sh" -l || return 1
 			setconfig "Lists=$config_lists_str"
 			call_script "$run_command" add -l "$config_lists_str"
@@ -338,7 +360,7 @@ restore_from_config() {
 	# call the *backup script to initiate recovery from fault
 	call_script "$install_dir/${p_name}-backup.sh" restore && check_reapply && return 0
 
-	die "Failed to restore the firewall state from backup. If it's a bug then please report it."
+	die "$FAIL restore the firewall state from backup. If it's a bug then please report it."
 }
 
 # tries to prevent the user from locking themselves out
@@ -347,15 +369,17 @@ check_for_lockout() {
 	[ ! "$user_ccode" ] && return 0
 	tip_msg="Make sure you do not lock yourself out."
 	u_ccode="country code '$user_ccode'"
+	inlist="in the planned $list_type"
+	trying="You are trying to"
 
 	if [ "$in_install" ]; then
 		get_matching_line "$planned_lists" "" "$user_ccode" "_*" filtered_ccode
 		case "$list_type" in
 			whitelist)
-				[ ! "$filtered_ccode" ] && lockout_msg="Your $u_ccode is not included in the planned whitelist. $tip_msg"
+				[ ! "$filtered_ccode" ] && lo_msg="Your $u_ccode is not included $inlist. $tip_msg"
 				return 0 ;;
 			blacklist)
-				[ "$filtered_ccode" ] && lockout_msg="Your $u_ccode is included in the planned blacklist. $tip_msg"
+				[ "$filtered_ccode" ] && lo_msg="Your $u_ccode is included $inlist. $tip_msg"
 				return 0
 		esac
 	else
@@ -365,13 +389,9 @@ check_for_lockout() {
 		[ ! "$filtered_ccode" ] && return 0
 
 		case "$action" in
-			add)
-				[ "$list_type" = blacklist ] && lockout_msg="You are trying to add your $u_ccode to the blacklist. $tip_msg"
-				return 0 ;;
-			remove)
-				[ "$list_type" = whitelist ] && lockout_msg="You are trying to remove your $u_ccode from the whitelist. $tip_msg"
-				return 0 ;;
-			*) printf '\n%s\n' "Error: Unexpected action '$action'." >&2; return 1
+			add) [ "$list_type" = blacklist ] && lo_msg="$trying add your $u_ccode to the blacklist. $tip_msg"; return 0 ;;
+			remove) [ "$list_type" = whitelist ] && lo_msg="$trying remove your $u_ccode from the whitelist. $tip_msg"; return 0 ;;
+			*) printf '\n%s\n' "$ERR Unexpected action '$action'." >&2; return 1
 		esac
 	fi
 }
@@ -390,7 +410,7 @@ for entry in "ListType list_type" "Families families" "Lists config_lists_str" "
 	getconfig "${entry% *}" "${entry#* }"
 done
 
-case "$list_type" in whitelist|blacklist) ;; *) die "Error: Unexpected geoip mode '$list_type'!"; esac
+case "$list_type" in whitelist|blacklist) ;; *) die "$ERR Unexpected geoip mode '$list_type'!"; esac
 
 san_str ccodes_arg "$(toupper "$ccodes_arg")"
 
@@ -410,10 +430,14 @@ run_command="$install_dir/${p_name}-run.sh"
 
 
 ## Check args for sanity
+
+erract="$ERR action '$action'"
+incompat="$erract is incompatible with option"
+
 case "$action" in
 	add|remove)
 		# check for valid country codes
-		[ ! "$ccodes_arg" ] && die "Error: action '$action' requires to specify countries with '-c <country_codes>'!"
+		[ ! "$ccodes_arg" ] && die "$erract requires to specify countries with '-c <country_codes>'!"
 		rv=0
 		for ccode in $ccodes_arg; do
 			validate_ccode "$ccode"
@@ -423,13 +447,13 @@ case "$action" in
 			esac
 		done
 
-		[ "$rv" != 0 ] && die "Invalid 2-letters country codes: '${bad_ccodes% }'."
-		;;
-	schedule|status|restore|reset|on|off|apply|showconfig) [ "$ccodes_arg" ] && die "Error: action '$action' is incompatible with option '-c'."
+		[ "$rv" != 0 ] && die "Invalid 2-letters country codes: '${bad_ccodes% }'." ;;
+	schedule|status|restore|reset|on|off|apply|showconfig)
+		[ "$ccodes_arg" ] && die "$incompat '-c'."
 esac
 
-[ "$action" != apply ] && [ "$ports_arg" ] && { usage; die "Action '$action' is incompatible with option '-p'."; }
-[ "$action" != schedule ] && [ "$cron_schedule" ] && { usage; die "Action '$action' is incompatible with option '-s'."; }
+[ "$action" != apply ] && [ "$ports_arg" ] && { usage; die "$incompat '-p'."; }
+[ "$action" != schedule ] && [ "$cron_schedule" ] && { usage; die "$incompat '-s'."; }
 
 
 
@@ -454,7 +478,7 @@ case "$action" in
 		# communicate schedule to *cronsetup via config
 		setconfig "CronSchedule=$cron_schedule"
 
-		call_script "$install_dir/${p_name}-cronsetup.sh" || die "Error: Failed to update cron jobs."
+		call_script "$install_dir/${p_name}-cronsetup.sh" || die "$ERR $FAIL update cron jobs."
 		exit 0
 esac
 
@@ -479,7 +503,7 @@ case "$action" in
 
 			[ "$wrong_lists" ] && {
 				get_wrong_ccodes
-				echo "NOTE: country codes '$wrong_ccodes' have already been added to the $list_type." >&2
+				printf '%s\n' "NOTE: country codes '$wrong_ccodes' have already been added to the $list_type." >&2
 			}
 		else
 			lists_to_change="$lists_arg"
@@ -495,7 +519,7 @@ case "$action" in
 			subtract_a_from_b "$config_lists" "$lists_arg" wrong_lists
 			[ "$wrong_lists" ] && {
 				get_wrong_ccodes
-				echo "NOTE: country codes '$wrong_ccodes' have not been added to the $list_type, so can not remove." >&2
+				printf '%s\n' "NOTE: country codes '$wrong_ccodes' have not been added to the $list_type, so can not remove." >&2
 			}
 		else
 			lists_to_change="$lists_arg"
@@ -520,8 +544,8 @@ fi
 
 nl2sp "$lists_to_change" lists_to_change_str
 
-if [ "$lockout_msg" ]; then
-		printf '\n%s\n\n%s\n' "${red}Warning${n_c}: $lockout_msg" "Proceed?"
+if [ "$lo_msg" ]; then
+		printf '\n%s\n\n%s\n' "$WARN $lo_msg" "Proceed?"
 		pick_opt "y|n"
 		case "$REPLY" in
 			y|Y) printf '\n%s\n' "Proceeding..." ;;
@@ -546,7 +570,7 @@ fi
 
 # positive return code means apply failure or another permanent error, except for 254
 case "$rv" in 0|254) ;; *)
-	echo "Error performing action '$action' for lists '$lists_to_change_str'." >&2
+	printf '%s\n' "Error performing action '$action' for lists '$lists_to_change_str'." >&2
 	[ ! "$config_lists" ] && die "Can not restore previous ip lists because they are not found in the config file."
 	# write previous config lists
 	setconfig "Lists=$config_lists_str"
@@ -558,7 +582,7 @@ subtract_a_from_b "$new_verified_lists" "$planned_lists" failed_lists
 if [ "$failed_lists" ]; then
 	nl2sp "$failed_lists" failed_lists_str
 	debugprint "planned_lists: '$planned_lists_str', new_verified_lists: '$new_verified_lists', failed_lists: '$failed_lists_str'."
-	echolog -err "Warning: failed to apply new $list_type rules for ip lists: $failed_lists_str."
+	echolog -err "$WARN failed to apply new $list_type rules for ip lists: $failed_lists_str."
 	# if the error encountered during installation, exit with error to fail the installation
 	[ "$in_install" ] && die
 	get_difference "$lists_to_change" "$failed_lists" ok_lists
