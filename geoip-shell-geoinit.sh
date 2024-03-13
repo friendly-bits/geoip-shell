ede19e6de4622d8e63fdc8acfc9432ef66d675d2
amos <anton.khazan@gmail.com>
2024-03-14 01:08:42 +0200

    geoinit: check deps rather than in -lib-common

--- geoip-shell-geoinit.sh
+++ geoip-shell-geoinit.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-# the install script makes a new version of this file with system-specific variables in the config dir
+# the install script makes a new version of this file
 
-lib_dir="$script_dir/lib"
-_lib="$lib_dir/$p_name-lib"
+export lib_dir="$script_dir/lib"
+export _lib="$lib_dir/$p_name-lib"
 . "${_lib}-check-compat.sh" || exit 1 # checks compatibility
 . "${_lib}-common.sh" || exit 1
+if [ "$(id -u)" = 0 ]; then
+	{ nolog=1 check_deps nft 2>/dev/null && export _fw_backend=nft; } ||
+	{ check_deps iptables ip6tables iptables-save ip6tables-save iptables-restore ip6tables-restore ipset && export _fw_backend=ipt
+	} || die "neither nftables nor iptables+ipset found."
+	nolog=
+fi
