98980b3e33f74fba89499d3937b2946f3752ed78
amos <anton.khazan@gmail.com>
2024-03-14 00:55:21 +0200

    check-ip-in-source: use the updated name for geoinit script

--- check-ip-in-source.sh
+++ check-ip-in-source.sh
@@ -11,7 +11,7 @@ p_name="geoip-shell"
 script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 
 export manmode=1
-. "$script_dir/${p_name}-init.sh" || exit 1
+. "$script_dir/${p_name}-geoinit.sh" || exit 1
 . "$_lib-ip-regex.sh"
 
 
@@ -26,7 +26,7 @@ For each of the specified ip addresses, checks whether it belongs to one of the 
     in the list fetched from a source (either RIPE or ipdeny) for a given country code.
 Accepts a mix of ipv4 and ipv6 addresses.
 
-Requires the 'grepcidr' utility, '${p_name}-fetch.sh', '${p_name}-common.sh', 'cca2.list'
+Requires the 'grepcidr' utility
 
 Options:
   -c <country_code>    : Country code (ISO 3166-1 alpha-2)
