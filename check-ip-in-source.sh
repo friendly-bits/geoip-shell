f3a178fa639393d17db9fe9edc589ee673ab3813
amos <anton.khazan@gmail.com>
2024-03-13 17:43:09 +0200

    source the -init script rather than -common

--- check-ip-in-source.sh
+++ check-ip-in-source.sh
@@ -11,7 +11,7 @@ p_name="geoip-shell"
 script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 
 export manmode=1
-. "$script_dir/${p_name}-common.sh" || exit 1
+. "$script_dir/${p_name}-init.sh" || exit 1
 . "$_lib-ip-regex.sh"
 
 
