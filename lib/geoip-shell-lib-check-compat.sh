f9c0b167061e41f4d98425e932b3fa275697b87c
amos <anton.khazan@gmail.com>
2024-03-16 18:36:08 +0200

    lib-check-compat: changed var name

--- lib/geoip-shell-lib-check-compat.sh
+++ lib/geoip-shell-lib-check-compat.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 # shellcheck disable=SC2006,SC2154,SC2010
 
+if [ -n "$curr_sh_g" ]; then return 0; fi
+
 # check for common deps
 for dep in grep tr cut sort wc awk sed logger pgrep; do
 	! command -v "$dep" 1>/dev/null && { echo "Error: missing dependency: '$dep'"; exit 1; }
 done
 
-fancy_sh="bash|ksh93"
 simple_sh="dash|ash|yash"
+fancy_sh="bash|ksh93"
 compat_sh="${simple_sh}|${fancy_sh}|busybox"
 
 
@@ -20,18 +22,18 @@ if [ ! -d "/proc" ]; then echo "Error: /proc not found."; exit 1; fi
 
 # check for supported shell
 if command -v readlink >/dev/null; then
-	curr_sh=`readlink /proc/$$/exe`
+	curr_sh_g=`readlink /proc/$$/exe`
 fi
-if [ -z "$curr_sh" ]; then
-	curr_sh=`ls -l /proc/$$/exe | grep -oE '/[^[:space:]]+$'`
+if [ -z "$curr_sh_g" ]; then
+	curr_sh_g=`ls -l /proc/$$/exe | grep -oE '/[^[:space:]]+$'`
 fi
-ok_sh=`echo "$curr_sh" | grep -E "/($compat_sh)"`
-if [ -z "$curr_sh" ]; then
+ok_sh=`printf %s "$curr_sh_g" | grep -E "/($compat_sh)"`
+if [ -z "$curr_sh_g" ]; then
 	echo "Warning: failed to identify current shell. $p_name may not work correctly. Please notify the developer." >&2
 elif [ -z "$ok_sh" ]; then
-	bad_sh=`echo "$curr_sh" | grep -E 'zsh|csh'`
+	bad_sh=`echo "$curr_sh_g" | grep -E 'zsh|csh'`
 	if [ -n "$bad_sh" ]; then
-		echo "Error: unsupported shell $curr_sh." >&2
+		echo "Error: unsupported shell $curr_sh_g." >&2
 		while [ "$n" -le 6 ]; do
 			___sh="${compat_sh%%"|"*}"
 			compat_sh="${compat_sh#*"|"}"
@@ -44,8 +46,9 @@ elif [ -z "$ok_sh" ]; then
 		done
 		exit 1
 	else
-		echo "Warning: whether $p_name works with your shell $curr_sh is currently unknown. Please test and notify the developer." >&2
+		echo "Warning: whether $p_name works with your shell $curr_sh_g is currently unknown. Please test and notify the developer." >&2
 	fi
 fi
-case "$curr_sh" in *busybox*) curr_sh="$curr_sh sh"; esac
+case "$curr_sh_g" in *busybox*) curr_sh_g="$curr_sh_g sh"; esac
+export curr_sh_g
 :
