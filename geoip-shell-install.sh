53b3aeaffb90cf9a087cb946a80ec0b477e1e75a
amos <anton.khazan@gmail.com>
2024-03-20 06:48:46 +0200

    install: bump the version to 0.3.1.1

--- geoip-shell-install.sh
+++ geoip-shell-install.sh
@@ -15,7 +15,7 @@
 
 #### Initial setup
 p_name="geoip-shell"
-curr_ver="0.3.1"
+curr_ver="0.3.1.1"
 script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 
 export manmode=1 in_install=1 nolog=1
