b9347016651035a3a7641d3b55dd4d63b6d795bd
amos <anton.khazan@gmail.com>
2024-03-21 12:49:02 +0200

    geoinit: minor logic simplification: always return 0 unless there is an error

--- geoip-shell-geoinit.sh
+++ geoip-shell-geoinit.sh
@@ -5,9 +5,9 @@ export lib_dir="$script_dir/lib"
 export _lib="$lib_dir/$p_name-lib"
 . "${_lib}-check-compat.sh" || exit 1 # checks compatibility
 . "${_lib}-common.sh" || exit 1
-if [ "$(id -u)" = 0 ]; then
-	{ nolog=1 check_deps nft 2>/dev/null && export _fw_backend=nft; } ||
-	{ check_deps iptables ip6tables iptables-save ip6tables-save iptables-restore ip6tables-restore ipset && export _fw_backend=ipt
-	} || die "neither nftables nor iptables+ipset found."
-	nolog=
-fi
+[ "$root_ok" ] || [ "$(id -u)" != 0 ] && return 0
+{ nolog=1 check_deps nft 2>/dev/null && export _fw_backend=nft; } ||
+{ check_deps iptables ip6tables iptables-save ip6tables-save iptables-restore ip6tables-restore ipset && export _fw_backend=ipt
+} || die "neither nftables nor iptables+ipset found."
+nolog=
+:
