e1a7f0cbff228139c408e1c970c22742b14fe78d
amos <anton.khazan@gmail.com>
2024-03-23 23:39:42 +0200

    geoinit: set some additional variables previously set in -common

--- geoip-shell-geoinit.sh
+++ geoip-shell-geoinit.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # the install script makes a new version of this file
 
-export lib_dir="$script_dir/lib"
-export _lib="$lib_dir/$p_name-lib" install_dir="/usr/bin"
+export install_dir="/usr/bin" lib_dir="$script_dir/lib" iplist_dir="/tmp" lock_file="/tmp/$p_name.lock"
+export _lib="$lib_dir/$p_name-lib" p_script="$script_dir/${p_name}" i_script="$inst_root_gs$install_dir/${p_name}" _nl='
+'
+export LC_ALL=C POSIXLY_CORRECT=yes default_IFS="	 $_nl"
+
 . "${_lib}-check-compat.sh" &&
 . "${_lib}-common.sh" || exit 1
 [ "$root_ok" ] || [ "$(id -u)" != 0 ] && return 0
