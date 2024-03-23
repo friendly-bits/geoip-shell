7709b5ae1a53959939e5a601ccbe314f63baa6ca
amos <anton.khazan@gmail.com>
2024-03-24 00:03:39 +0200

    lib-check-compat: - renamed some vars - support mksh, lksh shells - report posh as incompatible - recognize ksh93 as a fast shell

--- lib/geoip-shell-lib-check-compat.sh
+++ lib/geoip-shell-lib-check-compat.sh
@@ -8,9 +8,10 @@ for dep in grep tr cut sort wc awk sed logger pgrep; do
 	! command -v "$dep" 1>/dev/null && { echo "Error: missing dependency: '$dep'"; exit 1; }
 done
 
-simple_sh="dash|ash|yash"
-fancy_sh="bash|ksh93"
-compat_sh="${simple_sh}|${fancy_sh}|busybox"
+fast_sh="dash|ash|yash|ksh93|busybox sh|busybox"
+slow_sh="mksh|lksh|bash"
+compat_sh="${fast_sh}|${slow_sh}"
+incompat_sh="zsh|csh|posh"
 
 
 # not assuming a compatible shell at this point
@@ -21,23 +22,24 @@ if [ "$_g_test" != 11 ]; then echo "Error: grep doesn't support the required opt
 if [ ! -d "/proc" ]; then echo "Error: /proc not found."; exit 1; fi
 
 # check for supported shell
-if command -v readlink >/dev/null; then
+if command -v readlink 1>/dev/null; then
 	curr_sh_g=`readlink /proc/$$/exe`
 fi
 if [ -z "$curr_sh_g" ]; then
 	curr_sh_g=`ls -l /proc/$$/exe | grep -oE '/[^[:space:]]+$'`
 fi
+
 ok_sh=`printf %s "$curr_sh_g" | grep -E "/($compat_sh)"`
 if [ -z "$curr_sh_g" ]; then
 	echo "Warning: failed to identify current shell. $p_name may not work correctly. Please notify the developer." >&2
 elif [ -z "$ok_sh" ]; then
-	bad_sh=`echo "$curr_sh_g" | grep -E 'zsh|csh'`
+	bad_sh=`echo "$curr_sh_g" | grep -E "$incompat_sh"`
 	if [ -n "$bad_sh" ]; then
-		echo "Error: unsupported shell $curr_sh_g." >&2
+		echo "Error: incompatible shell $curr_sh_g." >&2
 		while [ "$n" -le 6 ]; do
 			___sh="${compat_sh%%"|"*}"
 			compat_sh="${compat_sh#*"|"}"
-			if command -v "$___sh" 1>/dev/null 2>/dev/null; then
+			if command -v "$___sh" 1>/dev/null; then
 				case "$___sh" in *busybox*) ___sh="busybox sh"; esac
 				echo "This system has a compatible shell '$___sh', you can use it with $p_name: '$___sh ${p_name}-install.sh'."
 				break
