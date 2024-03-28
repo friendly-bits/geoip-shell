0bd334b24c1b0c2720869dd893171d323b80b8db
amos <anton.khazan@gmail.com>
2024-03-28 03:59:34 +0200

    lib-owrt-common: - added enable_owrt_init() - moved functions rm_owrt_include(), rm_owrt_init(), restart_owrt_fw() from lib-uninstall

--- OpenWrt/geoip-shell-lib-owrt-common.sh
+++ OpenWrt/geoip-shell-lib-owrt-common.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
 
+# Copyright: friendly bits
+# github.com/friendly-bits
+
 # common functions and variables for OpenWrt-related scripts
 
 checkutil () { command -v "$1" 1>/dev/null; }
+
+enable_owrt_init() {
+	init_script="/etc/init.d/${p_name}-init"
+	if [ "$no_persist" ]; then
+		printf '%s\n\n' "Installed without persistence functionality."
+	else
+		! check_owrt_init && {
+			echo "Enabling the init script... "
+			$init_script enable
+		}
+		printf %s "Checking the init script... "
+		check_owrt_init || { echolog -err "$FAIL enable '$init_script'."; return 1; }
+		OK
+		! check_owrt_include && {
+			$init_script start
+			restart_owrt_fw
+			sleep 1
+		}
+		printf %s "Checking the firewall include... "
+		check_owrt_include || { echolog -err "$FAIL add firewall include."; return 1; }
+		OK
+	fi
+}
 
 check_owrt_init() {
 	set +f
@@ -23,6 +49,27 @@ check_owrt_include() {
 	check_uci_ent enabled 1 || return 1
 	[ "$_OWRTFW" = 4 ] && return 0
 	check_uci_ent reload 1
+}
+
+rm_owrt_fw_include() {
+	echo "Removing the firewall include..."
+	uci delete firewall."${p_name%%-*}_${p_name#*-}" 1>/dev/null 2>/dev/null
+
+	echo "Commiting fw$_OWRTFW changes..."
+	uci commit firewall
+	:
+}
+
+rm_owrt_init() {
+	echo "Deleting the init script..."
+	/etc/init.d/${p_name}-init disable 2>/dev/null && rm "/etc/init.d/${p_name}-init" 2>/dev/null
+	:
+}
+
+restart_owrt_fw() {
+	echo "Restarting the firewall..."
+	fw$_OWRTFW -q restart
+	:
 }
 
 me="${0##*/}"
