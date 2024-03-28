46b88e70e77e1f429a9046060f6ada674f9e1892
amos <anton.khazan@gmail.com>
2024-03-28 03:51:01 +0200

    lib-setup: - changed some arg names per updated config api - new function validate_arg_ccodes() - user_ccode config setting now always has a value - improved user input validation - use san_str() updated api - use subtract_a_from_b() updated api - pick_ifaces(): improved code comments - new function: set_defaults() - get_prefs(): use unified $in_setup flag - get_prefs: fixed some edge case with country codes selection - get_prefs(): initiate ifaces selection if they are unset and not provided via args - ifaces setting now always has a value - improved usage() hints

--- lib/geoip-shell-lib-setup.sh
+++ lib/geoip-shell-lib-setup.sh
@@ -12,9 +12,21 @@
 
 #### FUNCTIONS
 
+validate_arg_ccodes() {
+	bad_ccodes=
+	for ccode in $ccodes_arg; do
+		validate_ccode "$ccode"
+		case $? in
+			1) die "Internal error while validating country codes." ;;
+			2) bad_ccodes="$bad_ccodes$ccode "
+		esac
+	done
+	[ "$bad_ccodes" ] && die "Invalid 2-letters country codes: '${bad_ccodes% }'."
+}
+
 # checks country code by asking the user, then validates against known-good list
 pick_user_ccode() {
-	[ "$user_ccode_arg" = none ] || { [ "$nointeract" ] && [ ! "$user_ccode_arg" ]; } && { user_ccode=''; return 0; }
+	[ "$user_ccode_arg" = none ] || { [ "$nointeract" ] && [ ! "$user_ccode_arg" ]; } && { user_ccode=none; return 0; }
 
 	[ ! "$user_ccode_arg" ] && printf '\n%s\n%s\n' "${blue}Please enter your country code.$n_c" \
 		"It will be used to check if your geoip settings may block your own country and warn you if so."
@@ -25,8 +37,14 @@ pick_user_ccode() {
 			read -r REPLY
 		}
 		case "$REPLY" in
-			'') printf '%s\n\n' "Skipped."; return 0 ;;
-			*) toupper REPLY
+			'') printf '%s\n\n' "Skipped."; user_ccode=none; return 0 ;;
+			*)
+				is_alphanum "$REPLY" || {
+					REPLY=
+					[ "$nointeract" ] && die 1
+					continue
+				}
+				toupper REPLY
 				validate_ccode "$REPLY" "$script_dir/cca2.list"; rv=$?
 				case "$rv" in
 					0)  user_ccode="$REPLY"; break ;;
@@ -51,6 +69,12 @@ pick_ccodes() {
 			printf %s "Country codes (2 letters) or [a] to abort: "
 			read -r REPLY
 		}
+		case "$REPLY" in *[!A-Za-z\ ,\;]*)
+			printf '%s\n' "Invalid country codes '$REPLY'" >&2
+			REPLY=
+			[ "$nointeract" ] && die 1
+			continue
+		esac
 		toupper REPLY
 		trimsp REPLY
 		case "$REPLY" in
@@ -102,8 +126,8 @@ pick_ifaces() {
 		# fallback and non-OpenWRT
 		[ ! "$auto_ifaces" ] && auto_ifaces="$({ ip r get 1; ip -6 r get 1::; } 2>/dev/null |
 			sed 's/.*[[:space:]]dev[[:space:]][[:space:]]*//;s/[[:space:]].*//' | grep -vx 'lo')"
-		san_str auto_ifaces
-		get_intersection "$auto_ifaces" "$all_ifaces" auto_ifaces
+		san_str -n auto_ifaces
+		get_intersection "$auto_ifaces" "$all_ifaces" auto_ifaces "$_nl"
 		nl2sp auto_ifaces
 	}
 
@@ -130,14 +154,19 @@ pick_ifaces() {
 			read -r REPLY
 			case "$REPLY" in a|A) die 0; esac
 		}
-		san_str -s u_ifaces "$REPLY"
+		is_alphanum "$REPLY" || {
+				REPLY=
+				[ "$nointeract" ] && die
+				continue
+		}
+		san_str u_ifaces "$REPLY"
 		[ -z "$u_ifaces" ] && {
 			printf '%s\n' "No interface names detected in '$REPLY'." >&2
-			[ "$nointeract" ] && die
 			REPLY=
+			[ "$nointeract" ] && die
 			continue
 		}
-		subtract_a_from_b "$all_ifaces" "$u_ifaces" bad_ifaces ' '
+		subtract_a_from_b "$all_ifaces" "$u_ifaces" bad_ifaces
 		[ -z "$bad_ifaces" ] && break
 		echolog -err "Network interfaces '$bad_ifaces' do not exist in this system."
 		echo
@@ -150,13 +179,13 @@ pick_ifaces() {
 
 # 1 - input ip's/subnets
 # 2 - output var base name
-# output via $2_$family
-# if a subnet detected in ips of a particular family, output is prefixed with 'subnet: '
+# outputs whitespace-delimited validated ip's via $2_$family
+# if a subnet detected in ips of a particular family, output is prefixed with 'net:', otherwise with 'ip:'
 # expects the $families var to be set
 validate_arg_ips() {
 	va_ips_a=
 	sp2nl va_ips_i "$1"
-	san_str va_ips_i
+	san_str -n va_ips_i
 	[ ! "$va_ips_i" ] && { echolog -err "No ip's detected in '$1'."; return 1; }
 	for f in $families; do
 		unset "va_$f" ipset_type
@@ -168,7 +197,9 @@ validate_arg_ips() {
 		}
 		va_ips_a="$va_ips_a$va_ips_f$_nl"
 	done
-	subtract_a_from_b "$va_ips_a" "$va_ips_i" bad_ips || { nl2sp bad_ips; echolog -err "Invalid ip's: '$bad_ips'"; return 1; }
+	subtract_a_from_b "$va_ips_a" "$va_ips_i" bad_ips "$_nl" ||
+		{ nl2sp bad_ips; echolog -err "Invalid ip's: '$bad_ips'"; return 1; }
+
 	for f in $families; do
 		eval "${2}_$f=\"\$va_$f\""
 	done
@@ -227,8 +258,14 @@ pick_lan_ips() {
 					a|A) die 0
 				esac
 			}
-			san_str -s u_ips "$REPLY"
-			[ -z "$u_ips" ] && continue
+			case "$REPLY" in *[!A-Za-z0-9.:/\ ]*)
+				printf '%s\n' "Invalid ip's '$REPLY'"
+				REPLY=
+				[ "$nointeract" ] && die
+				continue
+			esac
+			san_str u_ips "$REPLY"
+			[ -z "$u_ips" ] && { [ "$nointeract" ] && die; continue; }
 			validate_ip "$u_ips" "$family" && break
 		done
 		confirm_ips
@@ -325,7 +362,7 @@ set_defaults() {
 
 	: "${nobackup:="$nobackup_def"}"
 	: "${datadir:="$datadir_def"}"
-	: "${schedule_conf:="15 4 * * *"}"
+	: "${schedule:="15 4 * * *"}"
 	: "${families:="ipv4 ipv6"}"
 	: "${geosource:="$geosource_def"}"
 	: "${tcp_ports:=skip}"
@@ -335,6 +372,10 @@ set_defaults() {
 }
 
 get_prefs() {
+	set_defaults
+
+	[ "$in_install" ] && in_setup=1
+
 	# nobackup
 	case "$nobackup_arg" in
 		''|true|false) ;;
@@ -344,16 +385,20 @@ get_prefs() {
 
 	# datadir
 	[ "$datadir_arg" ] && {
-		datadir_arg="${datadir_arg%/}"
-		[ ! "$datadir_arg" ] && die "Invalid directory '$datadir_arg'."
-		case "$datadir_arg" in */*) ;; *) die "Invalid directory '$datadir_arg'."; esac
-		parent_dir="${datadir_arg%/*}/"
+		datadir_new="${datadir_arg%/}"
+		[ ! "$datadir_new" ] && die "Invalid directory '$datadir_arg'."
+		case "$datadir_new" in */*) ;; *) die "Invalid directory '$datadir_arg'."; esac
+		[ "$datadir_new" != "$datadir" ] && {
+			{ find "$datadir_new" -mindepth 1 -maxdepth 1 | grep .; } 1>/dev/null 2>/dev/null &&
+				die "Can not create '$datadir_arg': it exists and is not empty."
+		}
+		parent_dir="${datadir_new%/*}/"
 		[ ! -d "$parent_dir" ] && die "Can not create '$datadir_arg': parent directory '$parent_dir' doesn't exist."
 	}
-	datadir="${datadir_arg:=$datadir}"
+	datadir="${datadir_new:=$datadir}"
 
 	# cron schedule
-	schedule_conf="${schedule_arg:=$schedule_conf}"
+	schedule="${schedule_arg:=$schedule}"
 
 	check_cron_compat
 	[ "$schedule_arg" ] && [ "$schedule_arg" != disable ] && {
@@ -387,7 +432,7 @@ get_prefs() {
 	[ "$ports_arg" ] && { setports "${ports_arg%"$_nl"}" || die; }
 
 	# geoip mode
-	[ "$geomode_arg" ] || [ "$in_install" ] && {
+	[ "$geomode_arg" ] || { [ ! "$geomode" ] && [ "$in_setup" ]; } && {
 		tolower geomode_arg
 		case "$geomode_arg" in
 			whitelist|blacklist) geomode="$geomode_arg" ;;
@@ -401,14 +446,14 @@ get_prefs() {
 		die "Option '-l' is incompatible with mode 'blacklist'."
 
 	# country codes
-	[ "$in_install" ] || [ "$geomode_change" ] && pick_ccodes
-	[ "$in_install" ] && pick_user_ccode
+	{ [ ! "$ccodes" ] && [ ! "$iplists" ]; } || [ "$geomode_change" ] || [ "$ccodes_arg" ] && pick_ccodes
+	[ ! "$user_ccode" ] && pick_user_ccode
 
 	# ifaces and lan subnets
 	lan_picked=
 	warn_lockout="${yellow}*NOTE*${n_c}: ${blue}In whitelist mode, traffic from your LAN subnets will be blocked, unless you whitelist them.$n_c"
 
-	if [ "$in_install" ] && [ -z "$ifaces_arg" ]; then
+	if [ ! "$ifaces" ] && [ -z "$ifaces_arg" ]; then
 		[ "$nointeract" ] && die "Specify interfaces with -i <\"ifaces\"|auto|all>."
 		printf '\n%s\n%s\n%s\n%s\n' "${blue}Does this machine have dedicated WAN network interface(s)?$n_c [y|n] or [a] to abort." \
 			"For example, a router or a virtual private server may have it." \
@@ -418,13 +463,13 @@ get_prefs() {
 		case "$REPLY" in
 			a|A) die 0 ;;
 			y|Y) pick_ifaces ;;
-			n|N) [ "$geomode" = whitelist ] && { printf '\n\n%s\n' "$warn_lockout"; pick_lan_ips; }
+			n|N) ifaces=all; [ "$geomode" = whitelist ] && { printf '\n\n%s\n' "$warn_lockout"; pick_lan_ips; }
 		esac
 	elif [ "$ifaces_arg" ]; then
 		ifaces=
 		case "$ifaces_arg" in
-			all) ifaces_arg=
-				[ "$geomode" = whitelist ] && { [ "$in_install" ] || [ "$geomode_change" ] || [ "$ifaces_change" ]; } &&
+			all) ifaces=all
+				[ "$geomode" = whitelist ] && { [ "$in_setup" ] || [ "$geomode_change" ] || [ "$ifaces_change" ]; } &&
 					{ printf '\n\n%s\n' "$warn_lockout"; pick_lan_ips; } ;;
 			auto) ifaces_arg=''; pick_ifaces -a ;;
 			*) pick_ifaces
@@ -449,36 +494,36 @@ geomode_usage="$mode_syn : Geoip blocking mode: whitelist or blacklist."
 if_syn="<\"[ifaces]\"|auto|all>"
 ifaces_usage="$if_syn :
 ${sp8}Changes which network interface(s) geoip firewall rules will be applied to.
-${sp8}'all' will apply geoip to all network interfaces
-${sp8}'auto' will autodetect WAN interfaces (this will cause problems if the machine has no direct WAN connection)
-${sp8}Generally, if the machine has dedicated WAN interfaces, specify them, otherwise pick 'all'.
-${sp8}Only works with the 'apply' action."
+${sp8}'all' will apply geoip to all network interfaces.
+${sp8}'auto' will automatically detect WAN interfaces (this may cause problems if the machine has no direct WAN connection).
+${sp8}Generally, if the machine has dedicated WAN interfaces, specify them, otherwise pick 'all'."
 
 lan_syn="<\"[lan_ips]\"|auto|none>"
 lan_ips_usage="$lan_syn :
 ${sp8}Specifies LAN ip's or subnets to exclude from geoip blocking (both ipv4 and ipv6).
-${sp8}Has no effect in blacklist mode.
+${sp8}Only compatible with whitelist mode.
 ${sp8}Generally, in whitelist mode, if the machine has no dedicated WAN interfaces,
-${sp8}specify LAN subnets to avoid blocking them. Otherwise you probably don't need this.
-${sp8}'auto' will autodetect LAN subnets during installation and at every update of the ip lists.
+${sp8}specify LAN ip's or subnets to avoid blocking them. Otherwise you probably don't need this.
+${sp8}'auto' will automatically detect LAN subnets during installation and at every update of the ip lists.
+${sp8}'none' removes previously set LAN ip's and disables the automatic detection.
 ${sp8}*Don't use 'auto' if the machine has a dedicated WAN interface*"
 
 tr_syn="<\"[trusted_ips]\"|none>"
 trusted_ips_usage="$tr_syn :
 ${sp8}Specifies trusted ip's or subnets to exclude from geoip blocking (both ipv4 and ipv6).
-${sp8}This option works independently from the above LAN ip's option.
-${sp8}Works both in whitelist and blacklist mode."
+${sp8}This option is independent from the above LAN ip's option.
+${sp8}Works both in whitelist and blacklist mode.
+${sp8}'none' removes previously set trusted ip's"
 
 ports_syn="<[tcp|udp]:[allow|block]:ports>"
 ports_usage="$ports_syn :
 ${sp8}For given protocol (tcp/udp), use 'block' to only geoblock incoming traffic on specific ports,
 ${sp8}or use 'allow' to geoblock all incoming traffic except on specific ports.
-${sp8}Multiple '-p' options are allowed to specify both tcp and udp in one command.
-${sp8}Only works with the 'apply' action."
+${sp8}To specify ports for both tcp and udp in one command, use the '-p' option twice."
 
 sch_syn="<\"[expression]\"|disable>"
 schedule_usage="$sch_syn :
-${sp8}Schedule expression for the periodic cron job implementing auto-updates of the ip lists, must be inside double quotes.
+${sp8}Schedule expression for the periodic cron job implementing automatic update of the ip lists, must be inside double quotes.
 ${sp8}Default schedule is \"15 4 * * *\" (at 4:15 [am] every day)
 ${sp8}'disable' will disable automatic updates of the ip lists."
 
