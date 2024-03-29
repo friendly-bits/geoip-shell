0bd73913c58d5b9f42bb97820cf93d5ebb23ecfc
amos <anton.khazan@gmail.com>
2024-03-29 04:15:28 +0300

    geoinit: check root before sourcing lib-common; use $fwbe_ok as a flag for firewall backend checks rather than $root_ok

--- geoip-shell-geoinit.sh
+++ geoip-shell-geoinit.sh
@@ -9,11 +9,16 @@ export _lib="$lib_dir/$p_name-lib" p_script="$script_dir/${p_name}" i_script="$i
 '
 export LC_ALL=C POSIXLY_CORRECT=yes default_IFS="	 $_nl"
 
-. "${_lib}-check-compat.sh" &&
+. "${_lib}-check-compat.sh" || exit 1
+[ "$root_ok" ] || { [ "$(id -u)" = 0 ] && export root_ok="1"; }
 . "${_lib}-common.sh" || exit 1
-[ "$root_ok" ] || [ "$(id -u)" != 0 ] && return 0
+
+# check firewall backend if we are root
+[ "$fwbe_ok" ] || [ ! "$root_ok" ] && return 0
+_no_l="$nolog"
 { nolog=1 check_deps nft 2>/dev/null && export _fw_backend=nft; } ||
 { check_deps iptables ip6tables iptables-save ip6tables-save iptables-restore ip6tables-restore ipset && export _fw_backend=ipt
 } || die "neither nftables nor iptables+ipset found."
-nolog=
+export fwbe_ok=1
+r_no_l
 :
