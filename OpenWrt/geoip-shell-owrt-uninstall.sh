b6e87fdf4aa8adc6fa552787f6b5a4b243eca10e
amos <anton.khazan@gmail.com>
2024-04-02 23:21:59 +0300

    owrt-uninstall: updated $lib_dir; compacted some code

--- OpenWrt/geoip-shell-owrt-uninstall.sh
+++ OpenWrt/geoip-shell-owrt-uninstall.sh
@@ -12,16 +12,17 @@ p_name="geoip-shell"
 manmode=1
 nolog=1
 
-lib_dir="/usr/lib"
+lib_dir="/usr/lib/$p_name"
 _lib="$lib_dir/$p_name-lib"
 
 geoinit="${p_name}-geoinit.sh"
 geoinit_path="/usr/bin/$geoinit"
 
 [ -f "$geoinit_path" ] && . "$geoinit_path"
-[ -f "$_lib-owrt-common.sh" ] && . "$_lib-owrt-common.sh"
-[ -f "$_lib-uninstall.sh" ] && . "$_lib-uninstall.sh"
-[ -f "$_lib-$_fw_backend.sh" ] && . "$_lib-$_fw_backend.sh"
+
+for lib_f in owrt-common uninstall "$_fw_backend"; do
+	[ -f "$_lib-$lib_f.sh" ] && . "$_lib-$lib_f.sh"
+done
 
 : "${conf_dir:=/etc/$p_name}"
 [ -d "$conf_dir" ] && : "${conf_file:="$conf_dir/$p_name.conf"}"
