88da091ceb0ce2d98da03180ef7759db5626edee
amos <anton.khazan@gmail.com>
2024-04-10 01:03:40 +0300

    remove libraries which were consolidated into lib-ipt, lib-nft

--- lib/geoip-shell-lib-ip-regex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ipv4_regex='((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])\.){3}(25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])'
-ipv6_regex='([0-9a-f]{0,4})(:[0-9a-f]{0,4}){2,7}'
-maskbits_regex_ipv4='(3[0-2]|([1-2][0-9])|[6-9])'
-maskbits_regex_ipv6='(12[0-8]|((1[0-1]|[1-9])[0-9])|[6-9])'
-subnet_regex_ipv4="${ipv4_regex}\/${maskbits_regex_ipv4}"
-subnet_regex_ipv6="${ipv6_regex}\/${maskbits_regex_ipv6}"
