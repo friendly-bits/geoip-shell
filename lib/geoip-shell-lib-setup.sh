1c718ee32c935a9dc9fcb03b1f6b5ba7e78c4836
amos <anton.khazan@gmail.com>
2024-04-22 12:34:45 +0300

    lib-setup: - improve code comments - set_defaults(): avoid unneccessary processing - set_defaults(): implement randomization of default update schedule minute (was always minute 15, now random between minute 10 and 20, except 13 to please the superstitious) - improve a usage var

--- lib/geoip-shell-lib-setup.sh
+++ lib/geoip-shell-lib-setup.sh
@@ -3,7 +3,7 @@
 
 # geoip-shell-lib-setup.sh
 
-# implements CLI interactive/noninteractive setup and args parsing for the -manage and -install scripts
+# implements CLI interactive/noninteractive setup and args parsing for the -manage script
 
 # Copyright: antonk (antonk.d3v@gmail.com)
 # github.com/friendly-bits
@@ -374,19 +374,30 @@ set_defaults() {
 		} 2>/dev/null
 	fi
 
-	# check RAM capacity, use performance optimization policy for nftables sets if RAM>=2048MiB
-	nft_perf_def=memory
-	IFS=': ' read -r _ memTotal _ < /proc/meminfo 2>/dev/null
-	case "$memTotal" in
-		''|*![0-9]*) ;;
-		*) [ $memTotal -ge 2097152 ] && nft_perf_def=performance
-	esac
+	# check RAM capacity, set default optimization policy for nftables sets to performance if RAM>=2048MiB
+	[ ! "$nft_perf" ] && {
+		nft_perf_def=memory
+		IFS=': ' read -r _ memTotal _ < /proc/meminfo 2>/dev/null
+		case "$memTotal" in
+			''|*![0-9]*) ;;
+			*) [ $memTotal -ge 2097152 ] && nft_perf_def=performance
+		esac
+	}
 
 	noblock_def=false no_persist_def=false
 
+	# randomly select schedule minute between 10 and 19
+	[ ! "$schedule" ] && {
+		rand_int="$(tr -cd 0-9 < /dev/urandom | dd bs=1 count=1 2>/dev/null)"
+		: "${rand_int:=0}"
+		# for the superstitious
+		[ "$rand_int" = 3 ] && rand_int=10
+		def_sch_minute=$((10+rand_int))
+	}
+
 	: "${nobackup:="$nobackup_def"}"
 	: "${datadir:="$datadir_def"}"
-	: "${schedule:="15 4 * * *"}"
+	: "${schedule:="$def_sch_minute 4 * * *"}"
 	: "${families:="ipv4 ipv6"}"
 	: "${geosource:="$geosource_def"}"
 	: "${_fw_backend:="$_fw_backend_def"}"
@@ -563,7 +574,7 @@ ${sp8}Specifies LAN ip's or subnets to exclude from geoip blocking (both ipv4 an
 ${sp8}Only compatible with whitelist mode.
 ${sp8}Generally, in whitelist mode, if the machine has no dedicated WAN interfaces,
 ${sp8}specify LAN ip's or subnets to avoid blocking them. Otherwise you probably don't need this.
-${sp8}'auto' will automatically detect LAN subnets during installation and at every update of the ip lists.
+${sp8}'auto' will automatically detect LAN subnets during the initial setup and at every update of the ip lists.
 ${sp8}'none' removes previously set LAN ip's and disables the automatic detection.
 ${sp8}*Don't use 'auto' if the machine has a dedicated WAN interface*"
 
