01eb5fc90a78c9619cea01bd31cbd7990e818179
amos <anton.khazan@gmail.com>
2024-11-13 19:34:55 +0200

    lib-common: - move check_fw_backend() from lib-non-wrt - setconfig(): print message when updating config - remove detect_fw_backend(), check_cron(), check_cron_compat(): now implemented separately in owrt and non-owrt libraries

--- lib/geoip-shell-lib-common.sh
+++ lib/geoip-shell-lib-common.sh
@@ -505,8 +505,9 @@ setconfig() {
 	# don't write to file if config didn't change
 	[ -f "$target_file" ] && old_conf_exists=1 || old_conf_exists=
 	if [ ! "$old_conf_exists" ] || ! compare_file2str "$target_file" "$newconfig"; then
-		[ "$debugmode" ] && [ "$target_file" = "$conf_file" ] && debugprint "${red}*** Updating config file ***${n_c}"
+		[ "$target_file" = "$conf_file" ] && printf %s "Updating the config file... " >&2
 		printf %s "$newconfig" > "$target_file" || { sc_failed "$FAIL write to '$target_file'"; return 1; }
+		[ "$target_file" = "$conf_file" ] && OK >&2
 	fi
 
 	[ "$target_file" = "$conf_file" ] && {
@@ -754,6 +755,20 @@ set_dir_vars() {
 	:
 }
 
+# return codes:
+# 0 - backend found
+# 1 - error
+# 2 - main backend not found (for ipt, iptables not found)
+# 3 - for ipt, ipset not found
+check_fw_backend() {
+	case "$1" in
+		nft) check_deps nft || return 2 ;;
+		ipt) check_deps iptables ip6tables iptables-save ip6tables-save iptables-restore ip6tables-restore || return 2
+			check_deps ipset || return 3 ;;
+		*) echolog -err "Unsupported firewall backend '$1'."; return 1
+	esac
+}
+
 # checks current ipsets and firewall rules for geoip-shell
 # returns a whitespace-delimited list of active ip lists
 # (optional: 1 - '-f' to force re-read of the table - nft-specific)
@@ -860,216 +875,8 @@ detect_ifaces() {
 	[ -r "/proc/net/dev" ] && sed -n '/^[[:space:]]*[^[:space:]]*:/{s/^[[:space:]]*//;s/:.*//p}' < /proc/net/dev | grep -vx 'lo'
 }
 
-detect_fw_backend() {
-	if [ "$_OWRTFW" ]; then
-		case "$_OWRTFW" in
-			3) printf ipt ;;
-			4) printf nft ;;
-			*) echolog -err "Invalid OpenWrt firewall version '$_OWRTFW'."; return 1
-		esac
-		return 0
-	fi
-
-	[ ! "$_fw_backend_arg" ] && {
-		. "$_lib-check-compat.sh" || { echolog -err "Failed to source '$_lib-check-compat.sh'."; return 1; }
-		if check_fw_backend nft 2>/dev/null; then
-			printf nft
-		else
-			check_fw_backend ipt 2>/dev/null
-			case $? in
-				0) printf ipt ;;
-				1) return 1 ;;
-				2) echolog -err "Neither nftables nor iptables not found."; return 1 ;;
-				3) echolog -err "Found iptables but utility 'ipset' not found."; return 1
-			esac
-		fi
-	}
-	:
-}
-
 try_read_crontab() {
 	crontab -u root -l 1>/dev/null 2>/dev/null
-}
-
-# returns 0 if crontab is readable and cron or crond process is running, 1 otherwise
-# sets $cron_reboot if above conditions are satisfied and cron is not implemented via the busybox binary
-check_cron() {
-	check_cron_path() {
-		cron_rl_path="$(ls -l "$1" 2>/dev/null)" || {
-			debugprint "Path '$1' not found"
-			return 1
-		}
-		debugprint "check_cron: Found real path: '/${cron_rl_path#*/}'."
-		# check for busybox cron
-		case "$cron_rl_path" in
-			*busybox*)
-				debugprint "Detected Busybox cron."
-				;;
-			*)
-				debugprint "Detected non-Busybox cron."
-				cron_reboot=1
-		esac
-		[ "$force_cron_persist" = true ] && {
-			debugprint "\$force_cron_persist is true."
-			cron_reboot=1
-		}
-		cron_rv=0
-		:
-	}
-
-	try_pidof() {
-		pidof "$1" 1>/dev/null && cron_path="$(command -v "$1")"
-	}
-
-	try_pgrep() {
-		cron_path="$(pgrep -af "/$1" | awk "BEGIN{rv=1} \$2 ~ /\/${1}\$/ {print \$2; rv=0; exit} END{exit rv}")"
-	}
-
-	debugprint "check_cron: \$no_persist is '$no_persist'. \$cron_rv is '$cron_rv'."
-	[ "$cron_rv" = 0 ] && return 0
-
-	unset cron_reboot cron_path
-	export cron_reboot cron_rv=1
-
-	# check for crontab command
-	checkutil crontab || {
-		debugprint "check_cron: crontab command not found."
-		cron_rv=3
-		return 3
-	}
-
-	# check reading crontab
-	try_read_crontab || {
-		debugprint "check_cron: $FAIL read crontab."
-		cron_rv=2
-		return 2
-	}
-
-	# check for cron or crond in running processes
-	for try_cmd in try_pidof try_pgrep; do
-		try_cmd_n="${try_cmd#try_}"
-		debugprint "check_cron: Trying with '${try_cmd_n}'..."
-		for cron_cmd in crond fcron cron; do
-			debugprint "Checking '$cron_cmd'"
-			if $try_cmd "$cron_cmd"; then
-				debugprint "${try_cmd_n} found '$cron_cmd', path: '$cron_path'"
-				check_cron_path "$cron_path"
-				case $? in
-					0) break 2 ;;
-					1) continue
-				esac
-			else
-				debugprint "${try_cmd_n} didn't find '$cron_cmd'"
-				continue
-			fi
-		done
-	done
-
-	debugprint "check_cron: returning '$cron_rv'"
-	return "$cron_rv"
-}
-
-# checks if the cron service is running and if it supports features required by the config
-# if cron service is not running, implements dialog with the user and optional automatic correction
-check_cron_compat() {
-	unset no_cr_persist cr_p1 cr_p2
-	[ ! "$_OWRTFW" ] && { cr_p1="s '-n true'"; cr_p2="persistence and "; }
-	[ "$no_persist" = true ] || [ "$_OWRTFW" ] && no_cr_persist=1
-	if [ "$schedule" != disable ] || [ ! "$no_cr_persist" ] ; then
-		i=0
-		while [ $i -le 1 ]; do
-			i=$((i+1))
-			# check if cron is running
-			check_cron && {
-				[ $i = 2 ] && {
-					OK
-					printf '%s\n%s\n%s' "Please restart the device after completing setup." \
-						"Then run '$p_name configure' and $p_name will check the cron service again." \
-						"Press Enter to continue "
-					read -r dummy
-				}
-				break
-			}
-			[ $i = 2 ] && { FAIL; die; }
-			case $cron_rv in
-				1)
-					cron_err_msg_1="cron is not running"
-					cron_err_msg_2="The cron service needs to be enabled and started in order for ${cr_p2}automatic ip list updates to work"
-					autosolution_msg="enable and start the cron service" ;;
-				2)
-					cron_err_msg_1="initial crontab file does not exist for user root"
-					cron_err_msg_2="The initial crontab file must exist so geoip-shell can create cron jobs for ${cr_p2}automatic ip list updates"
-					autosolution_msg="create the initial crontab file" ;;
-				3)
-					cron_err_msg_1="'crontab' utility not found. This usually means that cron is not installed."
-					cron_err_msg_2="cron is required for ${cr_p2}automatic ip list updates"
-			esac
-			echo
-			echolog -err "$cron_err_msg_1." "$cron_err_msg_2." \
-				"If you want to use $p_name without ${cr_p2}automatic ip list updates," \
-				"install/configure $p_name with option$cr_p1 '-s disable'."
-			[ "$nointeract" ] && {
-				[ ! "$_OWRTFW" ] && without_option=" without the option '-z'"
-				echolog "Please run '$p_name configure'$without_option in order to have $p_name enable the cron service for you."
-				die
-			}
-			[ "$cron_rv" = 3 ] && { echolog "Please install cron, then run '$p_name configure'."; die; }
-
-			printf '\n%s\n' "Would you like $p_name to $autosolution_msg? [y|n]."
-			pick_opt "y|n"
-			[ "$REPLY" = n ] && die
-
-			# if reading crontab fails, try to create an empty crontab
-			try_read_crontab || {
-				printf '\n%s' "Attempting to create a new crontab file for root... "
-				printf '' | crontab -u root - || { FAIL; die "command \"printf '' | crontab -u root -\" returned error code $?."; }
-				try_read_crontab || { FAIL; die "Issued crontab file creation command, still can not read crontab."; }
-				OK
-				if check_cron; then
-					break
-				else
-					i=0
-					continue
-				fi
-			}
-
-			# try to enable and start cron service
-			printf '\n%s' "Attempting to enable and start cron... "
-			debugprint "check_cron_compat: initsys is '$initsys'"
-			for cron_cmd in crond cron cronie fcron dcron; do
-				debugprint "check_cron_compat: trying '$cron_cmd'"
-				case "$initsys" in
-					systemd) systemctl status $cron_cmd; [ $? = 4 ] && continue
-							systemctl is-enabled "$cron_cmd" || systemctl enable "$cron_cmd"
-							systemctl start "$cron_cmd" ;;
-					sysvinit) checkutil update-rc.d && {
-								update-rc.d $cron_cmd enable
-								service $cron_cmd start; }
-							checkutil chkconfig && {
-								chkconfig $cron_cmd on
-								service $cron_cmd start; } ;;
-					upstart) rm -f "/etc/init/$cron_cmd.override" ;;
-					openrc) rc-update add $cron_cmd default || continue
-				esac
-
-				[ -f "/etc/init.d/$cron_cmd" ] && {
-					/etc/init.d/$cron_cmd enable
-					/etc/init.d/$cron_cmd start
-				}
-				check_cron && break
-			done 2>&1 |
-			if [ -n "$debugmode" ]; then cat 1>&2; else cat 1>/dev/null; fi
-		done
-
-		[ ! "$cron_reboot" ] && [ "$no_persist" != true ] && [ ! "$_OWRTFW" ] && {
-			echolog -err "Detected Busybox cron service. cron-based persistence may not work with Busybox cron on this device." \
-			"If you want to use $p_name without persistence support, run '$p_name configure -n true'." \
-			"If you want to force cron-based persistence support, run '$p_name configure -n false -P true'." \
-			"Reboot after installation and run 'geoip-shell status' to verify that persistence is working."
-			return 1
-		}
-	fi
-	:
 }
 
 OK() { printf '%s\n' "${green}Ok${n_c}."; }
