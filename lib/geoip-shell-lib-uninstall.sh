e4aff62610198e434376936fb9d22cc700b679f8
amos <anton.khazan@gmail.com>
2024-11-18 22:40:02 +0200

    lib-uninstall: kill_geo_pids(): rewrite

--- lib/geoip-shell-lib-uninstall.sh
+++ lib/geoip-shell-lib-uninstall.sh
@@ -9,31 +9,115 @@
 # github.com/friendly-bits
 
 
-# kills any running geoip-shell scripts
+# kills any running geoip-shell scripts and downloads
 kill_geo_pids() {
-	i_kgp=0 _parent="$(grep -o "${p_name}[^[:space:]]*" "/proc/$PPID/comm")"
-	while true; do
-		i_kgp=$((i_kgp+1)); _killed=
-		_geo_ps="$(pgrep -fa "(${p_name}\-|$ripe_url_stats|$ripe_url_api|$ipdeny_ipv4_url|$ipdeny_ipv6_url)" | grep -v pgrep)"
-		newifs "$_nl" kgp
-		for _p in $_geo_ps; do
-			_pid="${_p% *}"
-			_p="$p_name${_p##*"$p_name"}"
-			_p="${_p%% *}"
-			case "$_pid" in "$$"|"$PPID"|*[!0-9]*) continue; esac
-			[ "$_p" = "$_parent" ] && continue
-			IFS=' '
-			for g in run fetch apply cronsetup backup; do
-				case "$_p" in *${p_name}-$g*)
-					kill "$_pid" 2>/dev/null
-					_killed=1
+	get_ps_pid() {
+		_pid="${1%% *}"
+		case "$_pid" in
+			*[!0-9]*)
+				debugprint "get_ps_pid: invalid pid '$_pid'"
+				return 1 ;;
+		esac
+		:
+	}
+
+	kill_ps() {
+		debugprint "killing pid '$1' matching '$2'"
+		pgrep -fa "$1" | awk '{print $1 " " $2 " " $3}' >&2
+		kill "$1" 2>/dev/null
+		_killed=1
+	}
+
+	get_parents_pids() {
+		parent_pids="${$}|"
+		last_pid="${$}"
+		i_gpp=0
+		while [ $i_gpp -le 24 ]; do
+			i_gpp=$((i_gpp+1))
+			SPPID="$(
+				skipped='' prev_str=''
+				tr ' ' '\n' 2>/dev/null < "/proc/$last_pid/stat" | \
+				while IFS='' read -r str; do
+					case "$prev_str" in
+						*')'*) ;;
+						*) prev_str="$str"; continue
+					esac
+					[ ! "$skipped" ] && { skipped=1; continue; }
+					case "$str" in
+						*[!0-9]*) continue ;;
+						*) printf %s "$str"; break
+					esac
+				done
+			)"
+			[ ! "$SPPID" ] && break
+			last_pid="$SPPID"
+			parent_pids="${parent_pids}${SPPID}|"
+		done
+		printf %s "${parent_pids%|}"
+	}
+
+	all_parent_pids="$(get_parents_pids)"
+
+	printf '\n%s\n' "Killing any running $p_name processes..."
+
+	[ "$debugmode" ] && debugprint "parent pids: '$all_parent_pids'"
+
+	i_kgp=0
+	newifs "$default_IFS" kgp
+	while :; do
+		i_kgp=$((i_kgp+1))
+		_killed=
+
+		_geo_ps="$(
+			pgrep -fa "$p_name" | \
+			grep -Ev "(^${all_parent_pids}|(/usr/bin/)*$p_name(-manage.sh)* stop)[[:blank:]]|pgrep)" | \
+			grep -E "(^[[:blank:]]*[0-9][0-9]*${blanks}(sudo )*${p_name}|/usr/bin/${p_name}([^[:blank:]]*sh)*)([[:blank:]]|$)"
+		)"
+
+		[ "$debugmode" ] && debugprint "_geo_ps: '$(printf %s "$_geo_ps" | awk '{print $1 " " $2 " " $3 " " $4}')'"
+
+		_dl_ps="$(
+			pgrep -fa "($ripe_url_stats|$ripe_url_api|$ipdeny_ipv4_url|$ipdeny_ipv6_url)" | \
+			grep -v pgrep | grep -E '[[:blank:]](curl|wget|uclient-fetch)([[:blank:]]|$)'
+		)"
+
+		[ "$debugmode" ] && debugprint "_dl_ps: '$(printf %s "$_dl_ps" | awk '{print $1 " " $2 " " $3 " " $4}')'"
+
+		for g in "" manage run backup apply fetch cronsetup; do
+			kgp_script="${p_name}-${g}.sh"
+			[ ! "$g" ] && kgp_script="$p_name"
+
+			IFS="$_nl"
+			for entry in $_geo_ps; do
+				IFS="$default_IFS"
+				get_ps_pid "$entry" || continue
+
+				case "$entry" in "$kgp_script"|*" /usr/bin/$kgp_script "*|*" /usr/bin/$kgp_script"|"/usr/bin/$kgp_script "*)
+					kill_ps "$_pid" "$kgp_script"
+					continue 2
 				esac
 			done
 		done
-		oldifs kgp
-		[ ! "$_killed" ] && return 0
-		[ $i_kgp -gt 10 ] && { unisleep; return 0; }
+
+		IFS="$_nl"
+		for entry in $_dl_ps; do
+			IFS="$default_IFS"
+			get_ps_pid "$entry" || continue
+			for g in curl uclient-fetch wget; do
+				case "$entry" in *"$g"*)
+					kill_ps "$_pid" "$g"
+					continue 2
+				esac
+			done
+		done
+
+		if [ ! "$_killed" ] || [ $i_kgp -gt 30 ]; then
+			oldifs kgp
+			return 0
+		fi
+		sleep 1
 	done
+	oldifs kgp
 }
 
 rm_iplists_rules() {
