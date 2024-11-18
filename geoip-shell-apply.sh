42fa3d52c63ae756d746055dca81a83ddf7edadd
amos <anton.khazan@gmail.com>
2024-11-18 03:50:22 +0200

    apply: - remove direction-specific options - remove the 'remove' action - add the 'update' action - implement backend-agnostic get_ipset_id() - implement backend-agnostic get_ipset_name() - always source detect-lan when whitelist is present - fix lan automatic detection - implement logic to compute ipsets to keep, load, remove - always call get_counters before calling apply_rules() - avoid saving detected lan subnets in config unless apply_rules() succeeds

--- geoip-shell-apply.sh
+++ geoip-shell-apply.sh
@@ -20,19 +20,18 @@ set -- $_args; oldifs
 usage() {
 cat <<EOF
 
-Usage: $me <action> [-D $direction_syn -l <"list_ids">] [-s] [-d] [-V] [-h]
-Switches geoip blocking on/off, or loads/removes ip sets and firewall rules for specified lists.
+Usage: $me <action> [-s] [-d] [-V] [-h]
+Switches geolocking on/off, creates or modifies ipsets and firewall rules for configured ip lists.
 
 Actions:
-  on|off     :  Enable or disable the geoip blocking chain (via a rule in the base chain)
-  add|remove :  Add or remove ip sets and firewall rules for lists specified with the '-l' option
-  update     :  Update auxiliary geoblocking rules based on current config
+  on|off  :  Enable or disable the geoblocking chain (via a rule in the base chain)
+  add     :  Recreate geoblocking rules based on config and add missing ipsets, loading them from files
+  update  :  Recreate geoblocking rules based on config, loading ipsets from files
+  restore :  Recreate geoblocking rules based on config, re-using existing ipsets
+
+  Actions add, update and restore automatically remove ipsets which are no longer needed.
 
 Options:
-  -D $direction_syn : $direction_usage
-  -l <"list_ids">       : $list_ids_usage
-
-  -s : skip adding new ipsets (only create rules). Only for the iptables backend.
   -d : Debug
   -V : Version
   -h : This help
@@ -49,7 +48,7 @@ die_a() {
 # populates $counter_val for rule $1
 # 2 - family
 get_counter_val() {
-	[ "$counters_set" ] || return 0
+	[ "$counters_set" ] || { counter_val=''; return 0; }
 	enc_rule="$(printf %s "$1" | encode_rules -n "$2")"
 	case "$enc_rule" in
 		*[!A-Za-z0-9_]*)
@@ -67,7 +66,7 @@ get_counter_val() {
 			case "$counter_val" in
 				packets*bytes*)
 					# debugprint "counter val for rule '$1': '$counter_val'"
-					: ;;
+					;;
 				*)
 					debugprint "Invalid/empty counter val '$counter_val' for rule '$1', enc_rule '$enc_rule'"
 					counter_val="packets 0 bytes 0"
@@ -76,13 +75,46 @@ get_counter_val() {
 			case "$counter_val" in
 				\[*:*\])
 					# debugprint "counter val for rule '$1': '$counter_val'"
-					: ;;
+					;;
 				*)
 					debugprint "Invalid/empty counter val '$counter_val' for rule '$1', enc_rule '$enc_rule'"
 					counter_val="[0:0]"
 			esac
 	esac
 	:
+}
+
+# 1 - ipset name
+# assigns results to $list_id, $ipset_family
+get_ipset_id() {
+	case "$1" in *"_dhcp"*|*"_allow"*) i="$1" ;; *) i="${1%_*}"; esac
+	[ "$_fw_backend" = ipt ] && i="${i#*_}"
+	ipset_family="ipv${i##*_}"
+	list_id="${i%_*}_${ipset_family}"
+	case "$ipset_family" in
+		ipv4|ipv6) ;;
+		*) echolog -err "ip set name '$1' has unexpected format."
+			unset ipset_family list_id
+			return 1
+	esac
+	:
+}
+
+# 1 - var name
+# 2 - list id
+# assigns result to var named $1
+get_ipset_name() {
+	eval "list_date=\"\$prev_date_${list_id}\""
+	case "$1" in *"_dhcp"*|*"_allow"*) ;; *)
+		[ "$list_date" ] || {
+				echolog -err "The status file '$status_file' contains no information for list id '${list_id}'."
+				return 1
+		}
+		date_suffix="_${list_date}"
+	esac
+	[ "$2" ] || { echolog -err "get_ipset_name: list_id not specified"; return 1; }
+	[ "$_fw_backend" = ipt ] && prefix_gin="${p_name}_"
+	eval "$1=\"${prefix_gin}${2%%_*}_${2##*ipv}${date_suffix}\""
 }
 
 # 1 - direction
@@ -95,41 +127,16 @@ set_allow_ipset_vars() {
 
 ## PARSE ARGUMENTS
 
-parse_iplist_args() {
-	case "$action" in add|remove) ;; *) usage; die "Option '-l' can only be used with the 'add' and 'remove' actions."; esac
-	case "$direction" in
-		inbound|outbound)
-			eval "[ -n \"\$${direction}_list_ids_arg\" ]" && die "Option '-l' can not be used twice for direction '$direction'."
-			eval "${direction}_list_ids_arg"='$OPTARG' ;;
-		*) usage; die "Specify direction (inbound|outbound) to use with the '-l' option."
-	esac
-	req_direc_opt=
-}
-
 # check for valid action
 tolower action "$1"
 case "$action" in
-	add|remove|on|off|update) shift ;;
+	update|add|restore|on|off) shift ;;
 	*) unknownact
 esac
 
 # process the rest of the args
-unset skip_ipsets req_direc_opt
-while getopts ":D:l:sdVh" opt; do
+while getopts ":dVh" opt; do
 	case $opt in
-		D) case "$OPTARG" in
-				inbound|outbound)
-					case "$action" in add|remove) ;; *)
-						usage
-						die "Action is '$action', but direction-dependent options require the action to be 'add|remove'."
-					esac
-					[ "$req_direc_opt" ] && { usage; die "Provide valid options for the '$direction' direction."; }
-					direction="$OPTARG"
-					req_direc_opt=1 ;;
-				*) usage; die "Use 'inbound|outbound' with the '-D' option"
-			esac ;;
-		s) skip_ipsets=1 ;;
-		l) parse_iplist_args ;;
 		d) debugmode_arg=1 ;;
 		V) echo "$curr_ver"; exit 0 ;;
 		h) usage; exit 0 ;;
@@ -137,8 +144,6 @@ while getopts ":D:l:sdVh" opt; do
 	esac
 done
 shift $((OPTIND-1))
-
-[ "$req_direc_opt" ] && { usage; die "Provide valid options for direction '$direction'."; }
 
 extra_args "$@"
 
@@ -150,31 +155,8 @@ debugentermsg
 
 get_config_vars
 
-case "$inbound_list_ids_arg$outbound_list_ids_arg" in
-	'')
-		case "$action" in update|on|off) ;; *)
-			usage
-			die 254 "Specify iplist id's!"
-		esac ;;
-	*)
-		case "$action" in update|on|off)
-			usage
-			die 254 "Action '$action' is incompatible with option '-l'."
-		esac
-esac
-
-for direction in inbound outbound; do
-	eval "
-		if [ -n \"\$${direction}_list_ids_arg\" ]; then
-			${direction}_list_ids=\"\$${direction}_list_ids_arg\"
-		else
-			${direction}_list_ids=\"\$${direction}_iplists\"
-		fi"
-done
-
-debugprint "inbound list id's: '$inbound_list_ids', outbound list id's: '$outbound_list_ids'"
-
 geotag_aux="${geotag}_aux"
+
 
 ## CHECKS
 
@@ -192,8 +174,6 @@ checkvars datadir inbound_geomode outbound_geomode ifaces _fw_backend noblock ip
 debugprint "loading the $_fw_backend library..."
 . "$_lib-$_fw_backend.sh" || exit 1
 
-get_counters
-
 case "$action" in
 	on) geoip_on; exit ;;
 	off) geoip_off; exit
@@ -201,7 +181,7 @@ esac
 
 [ -n "$iplist_dir" ] && mkdir -p "$iplist_dir"
 
-is_whitelist_present && [ "$autodetect" ] &&
+is_whitelist_present &&
 	{ [ -s "${_lib}-detect-lan.sh" ] && . "${_lib}-detect-lan.sh" || die "$FAIL source the detect-lan script"; }
 
 
@@ -216,9 +196,9 @@ for family in $families; do
 
 		allow_ipset_name="allow_${direction%bound}_${family#ipv}"
 		allow_iplist_file="$iplist_dir/$allow_ipset_name.iplist"
-		eval "allow_ipset_name_${direction}_${family}=\"$allow_ipset_name\""
-		eval "allow_iplist_file_${direction}_${family}=\"$allow_iplist_file\""
-		eval "allow_ipset_type_${direction}_${family}=ip"
+		eval "allow_ipset_name_${direction}_${family}=\"$allow_ipset_name\"
+			allow_iplist_file_${direction}_${family}=\"$allow_iplist_file\"
+			allow_ipset_type_${direction}_${family}=ip"
 		allow_ipset_type=ip
 
 
@@ -229,7 +209,7 @@ for family in $families; do
 			allow_ipset_type=net
 
 			## load or detect lan ip's
-			if [ "$autodetect" ] && [ "$autodetected" ]; then
+			if [ "$autodetect" ] && [ ! "$autodetected" ]; then
 				res_subnets=
 				get_lan_subnets "$family" || die
 				autodetected=1
@@ -274,6 +254,8 @@ for family in $families; do
 
 		[ "$all_allow_ips" ] || continue
 
+		eval "allow_ipset_present_${direction}_${family}=1"
+
 		if [ "$all_allow_ips" != "$all_allow_ips_prev" ]; then
 			all_allow_ips_prev="$all_allow_ips"
 			allow_iplist_file_prev="$allow_iplist_file"
@@ -292,7 +274,7 @@ for family in $families; do
 
 			[ "$res_subnets" ] && {
 				printf '%s\n' "$res_subnets" > "$allow_iplist_file" || die "$FAIL write to file '$allow_iplist_file'"
-				debugprint "res_subnets:${_nl}'$res_subnets'"
+				debugprint "allow ip's:${_nl}'$res_subnets'"
 			}
 		else
 			# if allow ip's are identical for inbound and outbound, use same ipset for both
@@ -308,12 +290,87 @@ for family in $families; do
 	done
 done
 
+# generate lists:
+# planned_ipsets_$direction - will be used to create direction-specific rules for iplists
+# planned_ipsets - all planned final ipsets
+# rm_ipsets - ipsets to remove
+# load_ipsets - ipsets to load from file
+
+getstatus "$status_file" || die "$FAIL read the status file '$status_file'."
+
+curr_ipsets="$(get_ipsets)"
+
+nl2sp curr_ipsets_sp "$curr_ipsets"
+
+unset planned_ipsets rm1_ipsets load_ipsets
+for direction in inbound outbound; do
+	eval "
+		${direction}_list_ids=\"\$${direction}_iplists\"
+		list_ids=\"\$${direction}_list_ids\"
+		geomode=\"\$${direction}_geomode\""
+
+	debugprint "$direction list id's: '$list_ids'"
+
+	unset "planned_ipsets_$direction"
+
+	[ "$geomode" = disable ] && {
+		debugprint "$direction geomode is disable - skipping adding ipsets for it"
+		continue
+	}
+
+	# process list_ids
+	for list_id in $list_ids; do
+		case "$list_id" in [A-Z][A-Z]_ipv[46]) ;; *) die "Invalid iplist id '$list_id'."; esac
+
+		# set list_id-specific vars
+		family="${list_id#*_}"
+		list_id_short="${list_id%%_*}_${list_id##*ipv}"
+		get_ipset_name ipset "$list_id" || die
+
+		add2list "planned_ipsets_$direction" "$ipset"
+
+		case "$curr_ipsets_sp" in
+			# check for ipset with exactly matching name (for nft - with same list_id and date, for ipt - same list_id)
+			*"$ipset"*) [ "$action" = update ] && printf '%s\n' "Ip set for '$list_id' is already up-to-date." ;;
+			# check for ipset with different date - for nft only
+			*"$list_id_short"*)
+				case "$action" in add|restore) die "Detected ipset with unexpected name for list id '$list_id'."; esac
+				add2list rm1_ipsets "$ipset"
+		esac
+
+		add2list planned_ipsets "$ipset"
+	done
+done
+
+subtract_a_from_b "$planned_ipsets" "$curr_ipsets_sp" rm2_ipsets
+san_str rm_ipsets "$rm1_ipsets $rm2_ipsets"
+
+subtract_a_from_b "$rm_ipsets" "$curr_ipsets_sp" keep_ipsets
+subtract_a_from_b "$keep_ipsets" "$planned_ipsets" load_ipsets
+
+
+debugprint "curr_ipsets_sp: '$curr_ipsets_sp'"
+debugprint "planned ipsets: '$planned_ipsets'"
+debugprint "rm1 ipsets: '$rm1_ipsets'"
+debugprint "rm2 ipsets: '$rm2_ipsets'"
+debugprint "rm ipsets: '$rm_ipsets'"
+debugprint "keep ipsets: '$keep_ipsets'"
+debugprint "load ipsets: '$load_ipsets'"
+
+# check that there are iplist files to load ipsets from
+for ipset in $load_ipsets; do
+	get_ipset_id "$ipset"
+	[ -f "$iplist_dir/$list_id.iplist" ] || die "Can not find file '$iplist_dir/$list_id.iplist' to load ipset '$ipset'."
+done
+
+get_counters
+
 debugprint "calling apply_rules()"
 
 apply_rules
 rv_apply=$?
 
-[ "$autodetect" ] && {
+[ "$rv_apply" = 0 ] && [ "$autodetect" ] && {
 	[ "$lan_ips_ipv4" ] && setconf_lan=lan_ips_ipv4
 	[ "$lan_ips_ipv6" ] && setconf_lan="$setconf_lan lan_ips_ipv6"
 	[ "$setconf_lan" ] && setconfig $setconf_lan
