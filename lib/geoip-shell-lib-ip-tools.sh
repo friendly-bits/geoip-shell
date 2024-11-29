81ca18253a6c42ffcd121e3d500704dd4d9e1cd5
amos <anton.khazan@gmail.com>
2024-11-29 19:53:02 +0200

    lib-ip-tools: update comment

--- lib/geoip-shell-lib-ip-tools.sh
+++ lib/geoip-shell-lib-ip-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # shellcheck disable=SC2154,SC2086,SC2004
 
-# geoip-shell-lib-detect-lan.sh
+# geoip-shell-lib-ip-tools.sh
 
 # Copyright: antonk (antonk.d3v@gmail.com)
 # github.com/friendly-bits
