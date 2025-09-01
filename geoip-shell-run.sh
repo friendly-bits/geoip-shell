c8da5844a6e2b1e9806dccef8d9fcf1620431844
antonk <anton.khazan@gmail.com>
2025-09-01 17:12:28 +0300

    run: support custom script

--- geoip-shell-run.sh
+++ geoip-shell-run.sh
@@ -56,8 +56,8 @@ esac
 while getopts ":l:faodVh" opt; do
 	case $opt in
 		l)
-			[ "$action_run" = add ] || { usage; die "Option '-l' can only be used with the 'add' action."; }
-			[ "$lists_arg" ] && die "Option '-l' can not be used twice."
+			[ "$action_run" = add ] || { usage; run_fail 1 "Option '-l' can only be used with the 'add' action."; }
+			[ "$lists_arg" ] && run_fail 1 "Option '-l' can not be used twice."
 			lists_arg="$OPTARG" ;;
 		f) force_run="-f" ;;
 		a) export daemon_mode=1 ;;
@@ -73,7 +73,7 @@ shift $((OPTIND-1))
 extra_args "$@"
 
 is_root_ok
-. "$_lib-$_fw_backend.sh" || die
+. "$_lib-$_fw_backend.sh"
 
 setdebug
 debugentermsg
@@ -89,7 +89,7 @@ reg_last_update() {
 
 fetch_failed() {
 	resume_geoblocking
-	die "$@"
+	run_fail "$@"
 }
 
 resume_geoblocking() {
@@ -100,22 +100,59 @@ resume_geoblocking() {
 	}
 }
 
+run_success() {
+	[ -n "$*" ] && echolog "$@"
+	if [ -n "$CUSTOM_SCRIPT_OK" ]; then
+		(
+			. "$custom_script"
+			command -v gs_success 1>/dev/null || exit 0
+			session_log=
+			grep -iE '^(\[.*\])*[ 	]*(ERROR|WARNING):' "$GS_LOG_FILE" 1>/dev/null 2>&1 &&
+				session_log="$(get_session_log)"
+			gs_success "${session_log:+"Errors/warnings encountered.${_nl}Session log:${_nl}"}${session_log}"
+		)
+	fi
+	rm -f "${GS_LOG_FILE}"
+	die 0
+}
+
+run_fail() {
+	case "$1" in ''|*![0-9]*) ;; *) rf_rv="$1"; shift; esac
+	[ -n "$*" ] && echolog -err "$@"
+	if [ -n "$CUSTOM_SCRIPT_OK" ]; then
+		(
+			. "$custom_script"
+			command -v gs_failure 1>/dev/null || exit 0
+			session_log=
+			[ -s "$GS_LOG_FILE" ] && session_log="$(get_session_log)"
+			gs_failure "${session_log:+"Session log:${_nl}"}${session_log}"
+		)
+	fi
+	rm -f "${GS_LOG_FILE}"
+	die "${rf_rv:-1}"
+}
+
+
+rm -f "${GS_LOG_FILE}"
 
 #### VARIABLES
 
-export_conf=1 get_config_vars
+export_conf=1 nodie=1 get_config_vars || run_fail 1
+
+CUSTOM_SCRIPT_OK=
+[ -n "$custom_script" ] && [ -n "$daemon_mode" ] && check_custom_script "$custom_script" && CUSTOM_SCRIPT_OK=1
 
 nobackup="${nobackup_arg:-$nobackup}"
 
-san_str apply_lists_req "$lists_arg" || die
+san_str apply_lists_req "$lists_arg" || run_fail 1
 [ ! "$apply_lists_req" ] &&
 	case "$action_run" in
-		add) die "no list IDs were specified for actioin 'add'." ;;
+		add) run_fail 1 "no list IDs were specified for actioin 'add'." ;;
 		update|restore)
-			san_str apply_lists_req "$inbound_iplists $outbound_iplists" || die
+			san_str apply_lists_req "$inbound_iplists $outbound_iplists" || run_fail 1
 	esac
 
-separate_excl_iplists apply_lists_req "$apply_lists_req" || die
+separate_excl_iplists apply_lists_req "$apply_lists_req" || run_fail 1
 
 fast_el_cnt "$apply_lists_req" " " lists_cnt
 
@@ -127,20 +164,20 @@ failed_lists_cnt=0
 #### CHECKS
 
 checkvars i_script iplist_dir inbound_geomode outbound_geomode _fw_backend _lib
-check_deps "$i_script-fetch.sh" "$i_script-apply.sh" "$i_script-backup.sh" || die
+check_deps "$i_script-fetch.sh" "$i_script-apply.sh" "$i_script-backup.sh" || run_fail 1
 
 # check that the config file exists
-[ ! -f "$conf_file" ] && die "Config file '$conf_file' doesn't exist! Please run '$p_name configure'."
+[ ! -f "$conf_file" ] && run_fail 1 "Config file '$conf_file' doesn't exist! Please run '$p_name configure'."
 
 
 #### MAIN
 
-[ ! "$manmode" ] && echolog "Starting action '$action_run'."
+[ "$manmode" != 1 ] && echolog "Starting action '$action_run'."
 
-dir_mk -n "$iplist_dir" || die
+dir_mk -n "$iplist_dir" || run_fail 1
 
 mk_lock
-trap 'trap - INT TERM HUP QUIT; set +f; rm -f \"$iplist_dir/\"*.iplist; rm -f \"$fetch_res_file\"; die' INT TERM HUP QUIT
+trap 'trap - INT TERM HUP QUIT; set +f; rm -f \"$iplist_dir/\"*.iplist; rm -f \"$fetch_res_file\"; run_fail 1' INT TERM HUP QUIT
 
 # wait $reboot_sleep seconds after boot, or 0-59 seconds before updating
 [ "$daemon_mode" ] && {
@@ -166,7 +203,7 @@ case "$action_run" in
 		check_lists_coherence || force_run="-f" ;;
 	restore)
 		[ ! "$force_run" ] && check_lists_coherence -n 2>/dev/null &&
-			{ echolog "Geoblocking firewall rules and sets are Ok. Exiting."; die 0; }
+			run_success "Geoblocking firewall rules and sets are Ok. Exiting."
 		if [ "$nobackup" = true ]; then
 			echolog "$p_name was configured with 'nobackup' option, changing action to 'update'."
 			# if backup file doesn't exist, force re-fetch
@@ -174,8 +211,7 @@ case "$action_run" in
 		else
 			get_counters
 			if call_script -l "$i_script-backup.sh" restore -n && check_lists_coherence; then
-				echolog "Successfully restored IP lists."
-				die 0
+				run_success "Successfully restored IP lists."
 			else
 				# if restore failed, force re-fetch
 				echolog -err "Restore from backup failed. Changing action to 'update'."
@@ -201,7 +237,7 @@ unset all_fetched_lists missing_lists lists_fetch fetched_lists
 [ ! "$daemon_mode" ] && max_attempts=1
 case "$action_run" in add|update) lists_fetch="$apply_lists_req" ;; *) max_attempts=1; esac
 
-dir_mk "$datadir" || die
+dir_mk "$datadir" || run_fail 1
 
 attempt=0 secs=5
 resume_req=
@@ -212,7 +248,7 @@ if [ "$lists_fetch" ]; then
 		# 0 - geoip_off success, 2 - already off, 1 - error
 		case $? in
 			0) resume_req=1 ;;
-			1) die ;;
+			1) run_fail 1 "$FAIL pause outbound geoblocking." ;;
 			2)
 		esac
 	}
@@ -269,12 +305,11 @@ apply_rv=0
 get_intersection "$all_fetched_lists" "$apply_lists_req" all_apply_lists
 
 [ ! "$all_apply_lists" ] && {
-	echolog "Firewall reconfiguration isn't required."
 	if check_lists_coherence; then
 		reg_last_update
-		die 0
+		run_success "Firewall reconfiguration isn't required."
 	else
-		die 1
+		run_fail 1
 	fi
 }
 
@@ -287,21 +322,19 @@ set +f; rm -f "$iplist_dir/"*.iplist; set -f
 case "$apply_rv" in
 	0) ;;
 	254)
-		[ "$first_setup" ] && die
-		echolog -err "$p_name-apply.sh exited with code '254'. $FAIL execute action '$action_run'."
-		die 254 ;;
+		[ "$first_setup" ] && run_fail 1
+		run_fail 254 "$p_name-apply.sh exited with code '254'. $FAIL execute action '$action_run'." ;;
 	*)
 		debugprint "NOTE: apply exited with code '$apply_rv'."
-		die "$apply_rv"
+		run_fail "$apply_rv"
 esac
 
 if [ ! "$failed_lists" ] && check_lists_coherence; then
 	reg_last_update
-	echolog "Successfully ${print_action_done} IP lists '$apply_lists_req'."
-	echo
 	[ "$nobackup" = false ] && call_script -l "$i_script-backup.sh" create-backup
 else
-	die 1
+	run_fail 1
 fi
 
-die 0
+run_success "Successfully ${print_action_done} IP lists '$apply_lists_req'."
+echo
